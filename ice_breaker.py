cc78e60834637b3b1b702d43b6cbf4cb38613030
marco julio aparicio <mjaf83@gmail.com>
2025-03-31 19:27:01 +0100

    Initial commit ouput_parsers added

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,9 +1,9 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 from dotenv import load_dotenv
 
 language = "Spanish"
@@ -16,16 +16,20 @@ def ice_breaker_with(name: str) -> str:
         Given likedin information {information} a person that i want you to create in language {language}:
         1. A short summary.
         2. Two interesting facts about them.
+        \n{format_instructions}
         """
     summary_prompt_template = PromptTemplate(
         input_variables=["information", "language"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+            }
     )
 
     #llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     llm = ChatOllama(model="deepseek-r1:14b")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     res= chain.invoke(input={"information": linkedin_data, "language": language})
 
